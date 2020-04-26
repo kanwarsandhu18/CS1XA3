b63639510aaba398344d4d98831b328247290be1
Kanwar <kanwarsandhu1809@gmail.com>
2020-04-26 02:58:47 +0000

    Initial Project03 Commit

--- django_1/hello1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Hello1Config(AppConfig):
-    name = 'hello1'
--- django_1/hello1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def hello(request):
-        html = "<html> <body> HEllo World </body> </html>"
-        return HttpResponse(html)
-#def goodbye(request):
-#	html = "<html> <body> goodbye world </body> </html>"
-#        return HttpResponse(html)
